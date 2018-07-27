1668c833a4a5824f5e1936038b9e42ff569a2324
qwe14789cn <qwe14789cn@gmail.com>
2018-07-28 04:27:37 +0800

    fix error

--- +rt/write_data.m
+++ +rt/write_data.m
@@ -20,6 +20,7 @@
 %   rt.write_data(a,'%d ','a.txt')
 %--------------------------------------------------------------------------
 function write_data(data_name,data_type,file_name)
+[M,~] = size(data_name);
 f = fopen(file_name,'w');
 for idx = 1:size(data_name,2)
     fprintf(f,data_type,data_name(idx,:));
